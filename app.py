4d54bf80b72392110e1852a8c04dba93c9b3b0ef
anassmohamed54425-afk <anasmohamed54425@gmail.com>
2026-09-07 16:33:02 +0200

    download punkt_tab for nltk

--- app.py
+++ app.py
@@ -16,11 +16,21 @@ from collections import Counter
 import heapq
 
 # تحميل بيانات NLTK
+import ssl
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
+
 try:
     nltk.data.find('tokenizers/punkt')
 except LookupError:
     nltk.download('punkt')
     nltk.download('stopwords')
+    nltk.download('punkt_tab')
 
 # ========================================
 # دوال التلخيص والتصنيف (خفيفة)
